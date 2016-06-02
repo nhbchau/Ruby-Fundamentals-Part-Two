a21c4bbf0aea41d7bf04ed2e6f96059bd2f717cc
chau nguyen <nhbc111@yahoo.com>
2016-06-01 22:50:35 -0400

    added something

--- exercise6.rb
+++ exercise6.rb
@@ -1,12 +1,25 @@
 #grocery_list
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+                 #0         1               3         4
 #list each item in each line with '*'.
 grocery_list.each {|item| puts "* #{item}"}
-#forgot rice
+#forgot rice then need to add to the list.
 grocery_list << rice
 #get the number of item in the list
 grocery_list.length
 #=>5
 #pick banana if it included in the grocery list, don't need to pick if there is not.
-if grocery_list.include
-grocery_list.rassoc("banana")
+if grocery_list.include?("banana")
+  puts "You need to pick up banana!"
+else
+  puts "You don't need to pick up banana today!"
+end
+
+#Display the secound item in the grocery_list.
+grocery_list[1]
+
+#sort the grocery_list alphabetically
+grocery_list.sort {|item| puts "* #{item}"}
+
+#delete salmon from the grocery_list
+grocery_list.delete "salmon"
